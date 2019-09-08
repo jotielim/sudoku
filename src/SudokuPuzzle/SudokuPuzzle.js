e461bb87592c89e565c5c4ef199a723de9e61639
Yonathan Halim <jotielim@gmail.com>
2019-09-08 15:44:24 -0700

    Add SudokuPuzzle APIs

---
+++ src/SudokuPuzzle/SudokuPuzzle.js
@@ -0,0 +1,28 @@
+export default class SudokuPuzzle {
+  /**
+   * a means of populating the game board
+   */
+  setupBoard = () => {};
+
+  /**
+   * a means of checking the board to see if the puzzle has been successfully completed
+   */
+  checkBoard = () => {};
+
+  /**
+   * a player should be able to submit the puzzle at any time to have their progress checked
+   */
+  check = () => {};
+
+  /**
+   * a player should be able to give up on the puzzle and have the solution revealed
+   */
+  solve = () => {};
+
+  /**
+   * a player should be able to restart the game at any time; restarting should reset
+   * the board to the original state if you’ve implemented a single board, and should
+   * reset the board to an entirely new board if you’ve implemented a board generator
+   */
+  restart = () => {};
+}
